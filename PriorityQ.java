8829be89077aa9e8dd6ce3d8368f3ea0fac09bc6
QLi007 <50083210+QLi007@users.noreply.github.com>
2021-10-30 10:10:17 -0500

    commit om 'main'

--- PriorityQ.java
+++ PriorityQ.java
@@ -6,15 +6,14 @@ public class PriorityQ {
     public List<PriorityQ> list;
     private int index = 0;
     public List<Integer> indexList = new ArrayList<Integer>();
-
+    
+    //min heap
     PriorityQ(){
         list = new ArrayList<Priority>();
     }
-
     public Boolean isEmpty(){
         return list.isEmpty();
     }
-    
     public int getkey(int a){
         int size = list.size();
         if (a < size){
@@ -22,7 +21,6 @@ public class PriorityQ {
         }
         return -1;
     }
-
     public Vertex getValue(int a){
         int size = list.size();
         if (a < size){
@@ -30,9 +28,8 @@ public class PriorityQ {
         }
         return null;
     }
-
     private void sortDown(int i){
-        while(i < list.size() -1 ){
+        while(i < list.size() -1 && i > -1){
             if (left(i) == null && right(i) == null) break;
             Priority bigger;
             int big;
@@ -44,7 +41,6 @@ public class PriorityQ {
                 bigger = left(i);
                 big = i*2 + 1;
             }
-
             if (list.get(i).getKey() > list.get(big).getKey()) {
                 swap(i, big);
                 i = big;
@@ -52,7 +48,16 @@ public class PriorityQ {
             else break;
         }
     }
-    
+    private void sortup(int i){
+        while(i > -1 && i < list.size()){
+            if (parent(i) == null) break;
+            else if (list.get(i).getKey() < parent(i).get(i).getKey()){
+                int b = (i - 1)/2;
+                swap(i, b);
+                i = b;
+            }
+        }
+    }
     // add to list, then sort
     private void sortadd(){
         int a = list.size() -1;
@@ -63,23 +68,18 @@ public class PriorityQ {
             }
             else break;
         }
-
     }
-
     private void swap(int i , int j){
         Priority i1 = list.get(i);
         Priority j1 = list.get(j);
         //swap priority
         list.set(i,j1);
         list.set(j,i1);
-        
         //swap the index
         int indexi = i1.getName().getIndex();
         int indexj = j1.getName().getIndex();
-
         int ii = indexList.get(indexi);
         int jj = indexList.get(indexj);
-
         indexList.set(indexi, jj);
         indexList.set(indexj, ii);
     }
@@ -88,7 +88,6 @@ public class PriorityQ {
         if (i == 0) return list.get(i);
         return list.get((i-1/2));
     }
-
     private Priority left(int i){
         int a = i*2 + 1;
         if (a > list.size()) return null;
@@ -99,22 +98,18 @@ public class PriorityQ {
         if (a > list.size()) return null;
         return list.get(a);
     }
-
     public viod add(Vertex node, int key){
         Priority newNode = new Priority(node, key);
         newNode.getName().setIndex(index);
         list.add(newNode);
-
         indexList.add(index);
         sordadd();
         index++;
     }
-
     public Vertex returnMin(){
         if (list.isEmpty()) return null;
         return list.get(0).getName();
     }
-
     public Vertex extractMin(){
         if (list.isEmpty()) return null;
         Vertex res = list.get(0).getName();
@@ -123,7 +118,6 @@ public class PriorityQ {
         sortdown(0);
         return res;
     }
-
     public viod remove(int i){
         if (i > list.size() - 1) throw new NullPointerException();
         else {
@@ -132,20 +126,51 @@ public class PriorityQ {
             list.remove(list.size() - 1);
             sortdown(i);
         }
-
     }
-
-    public void decrementPriority(int i, int j){
-        
+    public void decrementPriority(int i, int k){
+        if (i > list.size() -1 || i<0) throw new NullPointerException();
+        else {
+            Vertex v = list.get(i).getName();
+            int key = list.get(i).getKey();
+            key -= k;
+            Priority res = new Priority(v, key);
+            list.set(i, res);
+            sortup(i);
+        }
     }
-
-
-
-
-
-
-
-
-
-
+    public void increasePriority(int i, int k){
+        if (i > list.size() - 1 || i < 0) throw new NullPointerException();
+        else {
+            Vertex v = list.get(i).getName();
+            int key = list.get(i).getKey();
+            key += k;
+            Priority res = new Priority(v, key);
+            list.set(i, res);
+            sortdown(i);
+        }
+    }
+    public void setPriority(int i, int k){
+        if (i > list.size() - 1 || i < 0) throw new NullPointerException();
+        else{
+            Vertex v = list.get(i).getName();
+            //int key = list.get(i).getKey();
+            v.setDis(k);
+            Priority res = new Priority(v,k);
+            list.set(i,res);
+            if (parent(i).getKey() > k) sortup(i); //key smaller than parent, sortup
+            //greater than either child, sortdown
+            else if (left(i).getKey() < k || right(i).getKey()<k) sortdown(i); //
+        }
+    }
+    public List<Priority> getList(){
+        return list;
+    }
+    public int[] priorityArray(){
+        if (list.isEmpty()) return null;
+        int [] res = new int[list.size()];
+        for(int i = 0;i< list.size();i++){
+            res[i] = list.get(i).getKey();
+        }
+        return res;
+    }
 }
